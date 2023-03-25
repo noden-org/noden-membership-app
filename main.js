a32831bc3f1bd405ff285437d9018a39fc225936
Raj Nakarja <raj@siliconwitchery.com>
2023-03-25 20:22:35 +0100

    Added first static content Connected API

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+
+require('dotenv').config()
 
 const PORT = process.env.PORT || 5100
 
 const app = express()
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 mins
+    max: 5
+})
+app.use(limiter)
+app.set('trust proxy', 1)
+
+// Set static folder
+app.use(express.static('public'))
+
+// Routes
+app.use('/api', require('./routes/routes.js'))
 
 // Enable cors
 app.use(cors())
