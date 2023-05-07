92ac2afe3abfa82f482f4f8642ecd1d4995a35e9
sigalor <nalenz@protonmail.com>
2023-05-07 22:57:38 +0200

    fix: frontend: correctly pass APPLICATION_ENV environment variable to Next.js

--- apps/frontend/src/app/config.tsx
+++ apps/frontend/src/app/config.tsx
@@ -18,7 +18,10 @@ const config: { [profileName in ConfigProfileName]: ApplicationConfig } = {
 
 export function getConfig() {
   // get APPLICATION_ENV environment variable from within Next.js
-  const env = (process.env.APPLICATION_ENV || 'local') as ConfigProfileName;
+  const env = process.env.NEXT_PUBLIC_APPLICATION_ENV as ConfigProfileName;
+  if (!env) {
+    throw new Error('NEXT_PUBLIC_APPLICATION_ENV environment variable not found');
+  }
   if (!config[env]) {
     throw new Error(`Config for environment '${env}' not found`);
   }
